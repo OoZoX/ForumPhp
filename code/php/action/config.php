f67e3f8e185c08dc7eb4971ed3c03e9b1c0edbae
dede95760 <bastienauffret95760@gmail.com>
2021-03-29 16:24:10 +0200

    hello quentin

--- code/php/action/config.php
+++ code/php/action/config.php
@@ -6,3 +6,8 @@ $pdo = new PDO(
   '',
   array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
   $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_WARNING);
+
+
+
+
+  /** hello quentn est ce que ca marche ? */
