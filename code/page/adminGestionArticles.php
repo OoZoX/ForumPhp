9f0839b47678ff8107e9eb02638d70ed830a2d5f
Quentin <bm.quentin@icloud.com>
2021-03-31 00:14:39 +0200

    orga,iz div

--- code/page/adminGestionArticles.php
+++ code/page/adminGestionArticles.php
@@ -3,20 +3,28 @@ require_once '../action/config.php';
 
 if (empty($_SESSION['admin'])) {
   header('Location: index.php');
-}
-else {
-  include '../content/nav.php';
-}
-?>
-<div>
-  <h1>Page Admin</h1>
+}?>
 
-</div>
+<div>
+  <div>
+    <?php include '../content/nav.php' ?>
+  </div>
+  <div>
+    <h1>Page Admin</h1>
 
-<?php include '../content/navAdmin.php'; ?>
-<div>
-  <h2>Creation d'un Article</h2>
-</div>
-<div>
-  <?php include '../content/formCreateArticles.php'; ?>
+  </div>
+
+  <?php include '../content/navAdmin.php'; ?>
+  <div>
+    <h2>Creation d'un Article</h2>
+  </div>
+  <div>
+    <div>
+      <?php include '../content/formCreateArticles.php'; ?>
+    </div>
+    <div>
+      <?php include '../' ?>
+    </div>
+  </div>
+
 </div>
