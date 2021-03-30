fb8ee928e9174467a1c5c5a5fbefacfc9d4df738
Quentin <bm.quentin@icloud.com>
2021-03-31 00:13:50 +0200

    reorganisation div

--- code/page/adminGestionPays.php
+++ code/page/adminGestionPays.php
@@ -3,22 +3,32 @@ require_once '../action/config.php';
 
 if (empty($_SESSION['admin'])) {
   header('Location: index.php');
-}
-else {
-  include '../content/nav.php';
-}
-?>
+}?>
+
 <div>
-  <h1>Page Admin</h1>
+  <div>
+    <?php include '../content/nav.php' ?>
+  </div>
 
-</div>
+  <div>
+    <h1>Page Admin</h1>
+  </div>
 
-<?php include '../content/navAdmin.php'; ?>
+  <div>
+    <?php include '../content/navAdmin.php'; ?>
+  </div>
 
-<h2>Creation Modification Suppression Pays</h2>
-<div>
-<?php
-include '../content/pays/formCreatePays.php';
-include '../content/pays/formModifPays.php';
-?>
+  <div>
+    <h2>Creation Modification Suppression Pays</h2>
+  </div>
+
+  <div>
+    <div>
+      <?php include '../content/pays/formCreatePays.php'; ?>
+    </div>
+
+    <div>
+      <?php include '../content/pays/formModifPays.php'; ?>
+    </div>
+  </div>
 </div>
