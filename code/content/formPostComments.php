3736ac2af0cdd3a85638b7e464059e4a3ceaffa1
Quentin <bm.quentin@icloud.com>
2021-03-31 06:11:54 +0200

    6

---
+++ code/content/formPostComments.php
@@ -0,0 +1,6 @@
+<form action="../action/postComment.php" method="post">
+  <input type="hidden" name="id_article" value="<?php echo $_RECUP['articleIndex']['0']['id'] ?>"/>
+  <input type="text" placeholder="Votre pseudo" name="pseuso" value=""/>
+  <textarea placeholder="commentaire" name="description" rows="8" cols="80"></textarea>
+  <input type="submit" name="btn" value="Poster"/>
+</form>
--- code/page/articles.php
+++ code/page/articles.php
@@ -12,5 +12,15 @@
       include '../content/afficheArticles.php' ;
       ?>
     </div>
+    <div>
+      <h2>Commentaires</h2>
+    </div>
+    <div>
+      <h3>Laisser un commentaire</h3>
+      <?php include '../content/formPostComments.php'; ?>
+    </div>
+    <div>
+      <h3>Tout les commentaires</h3>
+    </div>
   </body>
 </html>
