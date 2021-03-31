b047e1876ab0daec4b80d294fea0f1611e0618ab
Quentin <bm.quentin@icloud.com>
2021-03-31 02:50:29 +0200

    opti modif artciles

--- code/page/adminGestionArticles.php
+++ code/page/adminGestionArticles.php
@@ -42,37 +42,7 @@ if (empty($_SESSION['admin'])) {
     </div>
     <div>
 
-
-      <?php if (isset($_POST['countrySelect'])) :
-        include '../content/articles/recupArticles.php';
-        foreach ($dataArticle as $dataArticles) :?>
-        <form action="../action/gestionArticles.php" method="post">
-          <textarea name="title" placeholder="Titre Article" rows="8" cols="20"><?php echo $dataArticles['title'] ?></textarea>
-
-          <textarea name="date_content" placeholder="Date de l'evenement" rows="8" cols="20"><?php echo $dataArticles['date_content'] ?></textarea>
-
-          <select name="country">
-            <option value="test"><?php echo $_POST['countrySelect'] ?></option>
-            <?php include '../content/selectCountries.php'; ?>
-          </select>
-
-          <textarea name="location" placeholder="Ville" rows="8" cols="20"><?php echo $dataArticles['city'] ?></textarea>
-
-          <textarea name="text1" rows="8" cols="20"
-          placeholder="Premier Paragraphe"><?php echo $dataArticles['description'] ?></textarea>
-
-          <textarea name="imgArticle" accept=".jpg, .jpeg, .png, .gif" placeholder="Lien de l'image" rows="8" cols="20"><?php echo $dataArticles['image'] ?></textarea>
-
-          <textarea name="text2" rows="8" cols="20"
-          placeholder="Deuxième Paragraphe"><?php echo $dataArticles['description_2'] ?></textarea>
-
-          <input type="submit" name="Modifier" value="Modifier">
-          <input type="submit" name="Supprimer" value="Supprimer">
-
-        </form>
-      <?php endforeach;
-    endif;
-        ?>
+      <?php include '../content/articles/afficheArticlesAdmin.php'; ?>
 
 
     </div>
